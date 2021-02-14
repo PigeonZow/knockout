56d1ab9754a4e25b1fa50bcbf13531535a2358da
Patrick Zhao <pza28@sfu.ca>
2021-02-13 21:21:09 -0800

    organize code, add consts, console logging, sandbag button template

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,44 +1,67 @@
-var numPunches = 0;
+var numPunches = 100;
 var punchesPerClick = 1;
-var muscleBought = 0;
+
 var boxingGlovesBought = 0;
+var betterSandbagBought = 0;
+var muscleBought = 0;
+
+var punchesUntilBreak = 25;
+var sandbagDurability = 25;
+
+const MUSCLE_UPGRADE_PRICE = 10;
+const GLOVE_UPGRADE_PRICE = 300;
+const SANDBAG_UPGRADE_PRICE = 500;
 
 function punch() {
     numPunches += (punchesPerClick + punchesPerClick * boxingGlovesBought);
     var punchCounter = document.getElementById("punches")
     punchCounter.innerHTML = numPunches;
+
+    // if (punchesUntilBreak <= 0) {
+    //     breakPunchingBag();
+    // }
 }
 
-function notEnoughPunchesAnim() {
+// function breakPunchingBag() {
 
-}
+// }
 
 function buyMuscle() {
-    if (numPunches >= 10) {
-        numPunches -= 10;
-        var punchCounter = document.getElementById("punches")
-        punchCounter.innerHTML = numPunches;
+    if (numPunches >= MUSCLE_UPGRADE_PRICE) {
+        numPunches -= MUSCLE_UPGRADE_PRICE;
+        document.getElementById("punches").innerHTML = numPunches;
         muscleBought++;
         punchesPerClick += 1;
-        var punchMeter = document.getElementById("punchesPerClick") 
-        punchMeter.innerHTML = (punchesPerClick + punchesPerClick * boxingGlovesBought);
+        document.getElementById("punchesPerClick").innerHTML = (punchesPerClick + punchesPerClick * boxingGlovesBought);
+        console.log("bought muscle upgrade");
     } else {
-        console.log("not enough punches for muscle")
-        alert("Not enough punches.") // temp
+        alert("Not enough punches."); // temp
+        console.log("not enough punches for muscle upgrade"); // debug
     }
 }
 
 function buyGloves() {
-    if (numPunches >= 300 && boxingGlovesBought != 1) {
-        numPunches -= 300;
-        var punchCounter = document.getElementById("punches")
-        punchCounter.innerHTML = numPunches;
+    if (numPunches >= GLOVE_UPGRADE_PRICE && !boxingGlovesBought) {
+        numPunches -= GLOVE_UPGRADE_PRICE;
+        document.getElementById("punches").innerHTML = numPunches;
         boxingGlovesBought = 1;
-        var punchMeter = document.getElementById("punchesPerClick") 
-        punchMeter.innerHTML = (punchesPerClick + punchesPerClick * boxingGlovesBought);
+        document.getElementById("punchesPerClick").innerHTML = (punchesPerClick + punchesPerClick * boxingGlovesBought);
         document.getElementById("boxingGlovesBuy").disabled = true;
+        console.log("bought glove upgrade"); 
     } else {
-        console.log("not enough punches for gloves")
-        alert("Not enough punches.")
+        console.log("not enough punches for gloves upgrade"); // debug
+        alert("Not enough punches."); // temp
     }
 }
+
+function buySandbag() {
+    if (numPunches >= SANDBAG_UPGRADE_PRICE && !betterSandbagBought) {
+        numPunches -= SANDBAG_UPGRADE_PRICE;
+        console.log("bought sandbag upgrade");
+        betterSandbagBought = 1;
+    } else {
+        console.log("not enough punches for sandbag upgrade"); // debug
+        alert("Not enough punches."); // temp
+    }
+}
+
